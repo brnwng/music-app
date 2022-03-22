b64297f2b52380f116d33b126c182cc5f48014b9
Brian Wong <bwjing@gmail.com>
2022-03-21 22:12:50 -0700

    clean up

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -20,6 +20,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     } catch (e) {
         res.status(401);
         res.json({ error: 'User already exists' });
+        return;
     }
 
     const token = jwt.sign(
