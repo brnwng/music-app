05435327883e0e84c7d4807905e99c5d62c93cee
Brian Wong <bwjing@gmail.com>
2022-03-28 21:55:12 -0700

    add song handlers

--- components/songsTable.tsx
+++ components/songsTable.tsx
@@ -3,8 +3,17 @@ import { Table, Thead, Td, Tr, Tbody, Th, IconButton } from '@chakra-ui/react';
 import { BsFillPlayFill } from 'react-icons/bs';
 import { AiOutlineClockCircle } from 'react-icons/ai';
 import { formatDate, formatTime } from '../lib/formatters';
+import { useStoreActions } from 'easy-peasy';
 
 const SongTable = ({ songs }) => {
+    const playSongs = useStoreActions((store: any) => store.changeActiveSongs);
+    const setActiveSong = useStoreActions((store: any) => store.changeActiveSong);
+
+    const handlePlay = (activeSong?) => {
+        setActiveSong(activeSong || songs[0]);
+        playSongs(songs);
+    }
+
     return (
         <Box bg='transparent' color='white'>
             <Box padding='10px' marginBottom='20px'>
@@ -14,7 +23,9 @@ const SongTable = ({ songs }) => {
                         aria-label='play'
                         colorScheme='green'
                         size='lg'
-                        isRound />
+                        isRound
+                        onClick={() => handlePlay()}
+                    />
                 </Box>
                 <Table variant='unstyled'>
                     <Thead borderBottom='1px solid' borderColor='rgba(255, 255, 255, 0.2)'>
@@ -36,7 +47,8 @@ const SongTable = ({ songs }) => {
                                 },
                             }}
                                 key={song.id}
-                                cursor='cursor'
+                                cursor='pointer'
+                                onClick={() => handlePlay(song)}
                             >
                                 <Td> {i + 1} </Td>
                                 <Td> {song.name} </Td>
